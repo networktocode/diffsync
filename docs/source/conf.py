ef16caeb7e8ab287b0a797230af887df2b9bff46
Josh Silvas <josh@jsilvas.com>
2021-06-21 20:32:28 -0500

    Adds small changes for tests.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,15 +1,16 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""Configuration file for the Sphinx documentation builder.
+
+This file only contains a selection of the most common options. For a full
+list see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
 
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
+# pylint: disable=W,C,R
 import os
 import sys
 
@@ -36,7 +37,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 
 project = TOOL_CONFIG["name"]
 copyright = f"2021, {','.join(TOOL_CONFIG['authors'])}"
-author = ','.join(TOOL_CONFIG['authors'])
+author = ",".join(TOOL_CONFIG["authors"])
 
 # The full version, including alpha/beta/rc tags
 release = TOOL_CONFIG["version"]
@@ -47,11 +48,7 @@ release = TOOL_CONFIG["version"]
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "m2r2"
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "m2r2"]
 
 autodoc_default_options = {
     "members": True,
@@ -62,12 +59,12 @@ autodoc_default_options = {
 
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['templates']
+templates_path = ["templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -75,16 +72,16 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['static']
+html_static_path = ["static"]
 
 
 def remove_module_docstring(app, what, name, obj, options, lines):
-    """Removes copyright information at the top of each module to prevent unneeded reference in the API documentation"""
+    """Removes copyright heading on modules to prevent unneeded reference in the API documentation."""
     if what == "module":
         # At the module level, remove everything except the first line containing a summary of the module. All
         # lines that follow are copyright notices.
