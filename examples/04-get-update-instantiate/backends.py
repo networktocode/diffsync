0bb0f0e7c16460756c850289a532045f4b850491
Mikhail Yohman <mikhail.yohman@gmail.com>
2021-11-20 16:11:10 -0700

    Pylint fails within the pipeline, but not locally.

--- examples/04-get-update-instantiate/backends.py
+++ examples/04-get-update-instantiate/backends.py
@@ -15,7 +15,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from models import Site, Device, Interface
+from models import Site, Device, Interface  # pylint: disable=no-name-in-module
 from diffsync import DiffSync
 
 BACKEND_DATA_A = [
