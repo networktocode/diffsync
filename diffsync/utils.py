fffae739dbf67a3ec9bd756b0321624eb81dde37
Ken Celenza <ken@celenza.org>
2022-11-02 05:47:27 -0400

    Update docker, add methods to load from dictionary, get tree traversa… (#174)
    * Update docker, add methods to load from dictionary, get tree traversal, and update docs
    
    * Apply suggestions from code review
    
    Co-authored-by: Glenn Matthews <glenn.matthews@networktocode.com>
    
    * Additional recommended updates for loop efficiency and using classmethod
    
    * update docs
    
    * mypy feedback
    
    * lock poetry
    
    Co-authored-by: Glenn Matthews <glenn.matthews@networktocode.com>

--- diffsync/utils.py
+++ diffsync/utils.py
@@ -16,7 +16,12 @@ limitations under the License.
 """
 
 from collections import OrderedDict
-from typing import List
+from typing import Iterator, List, Dict, Optional
+
+SPACE = "    "
+BRANCH = "│   "
+TEE = "├── "
+LAST = "└── "
 
 
 def intersection(lst1, lst2) -> List:
@@ -42,3 +47,44 @@ class OrderedDefaultDict(OrderedDict):
         """When trying to access a nonexistent key, initialize the key value based on the internal factory."""
         self[key] = value = self.factory()
         return value
+
+
+# from: https://stackoverflow.com/questions/72618673/list-directory-tree-structure-in-python-from-a-list-of-path-file
+def _tree(data: Dict, prefix: str = "") -> Iterator[str]:
+    """Given a dictionary will yield a visual tree structure.
+
+    A recursive generator, given a dictionary will yield a visual tree structure line by line
+    with each line prefixed by the same characters.
+    """
+    # contents each get pointers that are ├── with a final └── :
+    pointers = [TEE] * (len(data) - 1) + [LAST]
+    for pointer, path in zip(pointers, data):
+        yield prefix + pointer + path
+        if isinstance(data[path], dict):  # extend the prefix and recurse:
+            extension = BRANCH if pointer == TEE else SPACE
+            # i.e. SPACE because LAST, └── , above so no more |
+            yield from _tree(data[path], prefix=prefix + extension)
+
+
+def tree_string(data: Dict, root: str) -> str:
+    """String wrapper around `_tree` function to add header and provide tree view of a dictionary."""
+    return "\n".join([root, *_tree(data)])
+
+
+def set_key(data: Dict, keys: List):
+    """Set a nested dictionary key given a list of keys."""
+    current_level = data
+    for key in keys:
+        current_level = current_level.setdefault(key, {})
+
+
+def get_path(nested_dict: Dict, search_value: str) -> Optional[List]:
+    """Find the path of keys in a dictionary, given a single unique value."""
+    for key in nested_dict.keys():
+        if key == search_value:
+            return [key]
+        if isinstance(nested_dict[key], dict):
+            path = get_path(nested_dict[key], search_value)
+            if path is not None:
+                return [key] + path
+    return None
